931e6f2f9893f7be4c7b3289cf2f2f1d62aeddb7
877942720@qq.com <lsj890305>
2018-03-19 12:40:16 +0800

    13-2

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,17 +1,11 @@
 import { Cart } from '../cart/cart-model.js';
+import { Address } from '../../utils/address.js';
 var cart = new Cart();
+var address = new Address();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
   
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     var flag = options.from == 'cart',
       that = this;
@@ -30,53 +24,30 @@ Page({
       this.data.id = options.id;
     }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  editAddress: function () {
+    var that = this;
+    wx.chooseAddress({
+      success: function (res) {
+        var addressInfo = {
+          name: res.userName,
+          mobile: res.telNumber,
+          totalDetail: address.setAddressInfo(res)
+        };
+        that._bindAddressInfo(addressInfo);
+        // address.submitAddress(res, (flag) => {
+        //   if (!flag) {
+        //     that.showTips('操作提示', '地址信息更新失败！');
+        //   }
+        // });
+      },
+      fail: function (res){
+      } 
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  /*绑定地址信息*/
+  _bindAddressInfo: function (addressInfo) {
+    this.setData({
+      addressInfo: addressInfo
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
